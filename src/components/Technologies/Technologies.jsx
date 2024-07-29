e5bd20b7ce2597b847e7d5fca0b61708661d5b20
NeilTheSeal <neil.hendren@gmail.com>
2024-07-29 11:18:22 -0600

    Add improved parallax

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -1,10 +1,25 @@
+import { useEffect } from "react";
 import "./Technologies.css";
 import TechContainer from "/src/components/TechContainer/TechContainer.jsx";
 import TechnologiesData from "/src/data/technologies.js";
 
 export default function Technologies() {
+  useEffect(() => {
+    const bg = document.getElementById("technologies-bg");
+    let scroll = window.scrollY;
+    window.addEventListener("scroll", () => {
+      if (scroll < window.scrollY) {
+        bg.style.backgroundPosition = `0 ${window.scrollY / 2}px`;
+      } else {
+        bg.style.backgroundPosition = `0 ${window.scrollY / 2}px`;
+      }
+      scroll = window.scrollY;
+    });
+  }, []);
+
   return (
     <section id="technologies">
+      <div id="technologies-bg"></div>
       <h2 className="technologies-header">Technologies I Use</h2>
       <div className="technologies-backdrop">
         <TechContainer
