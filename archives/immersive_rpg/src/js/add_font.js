8756ef7c72ff797dbfbfb237cf29792ffe1970b8
NeilTheSeal <neil.hendren@gmail.com>
2024-11-16 11:33:41 -0700

    Move archives to github pages

--- archives/immersive_rpg/src/js/add_font.js
+++
@@ -1,6 +0,0 @@
-const head = document.head;
-const href = "resources/style/font.css";
-const link = document.createElement("link");
-link.setAttribute("rel", "stylesheet");
-link.setAttribute("href", href);
-head.appendChild(link);
--- archives/immersive_rpg/src/js/load_assets.js
+++
@@ -1,6 +0,0 @@
-function preload() {
-  g.assets.models.man = loadModel("./resources/models/man.obj");
-  g.assets.textures.ground = loadImage("./resources/images/ground_tile_1.jpg");
-}
-
-module.exports = preload;
