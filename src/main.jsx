b8008a6b6537cbe7f4054e3800317ab245ca3ccc
NeilTheSeal <neil.hendren@gmail.com>
2025-11-15 00:21:26 -0700

    Add favicon

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
+import Icon from "./assets/favicon.ico";
 import NavBar from "./components/NavBar/NavBar.jsx";
 import "./index.css";
 import routes from "./routes/routes.js";
+
+const icon = document.createElement("link");
+icon.rel = "icon";
+icon.href = Icon;
+
+document.head.appendChild(icon);
 
 const browserRoutes = routes.map((route) => ({
   path: route.uri,
